a1ac0b57ea4f72a3a8a5a47b8d897067d1832cc8
Francesco Tullii <francesco.tullii@fcagroup.com>
2020-03-26 11:19:28 +0100

    working JMT

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -165,26 +165,43 @@ vector<vector<double>> PathPlanner::generate_trajectory(Car &car){
         next_y_vals.push_back(car.previous_path_y[i]);
     }
 
-    // Calculate how to break up spline points
+    // Initial conditions
+    vector<double> start {0., ref_vel/2.24, 0.};
+
+    // Target
+    double t_add_on = 0;
     double target_x = 20;
     double target_y = s(target_x);
-    double target_dist = sqrt(pow(target_x,2) + pow(target_y, 2));
-    double x_add_on = 0;
+    double target_dist = sqrt(pow(target_x,2) + pow(target_y,2));
 
-    for(int i = 1; i <= 50-car.previous_path_size; ++i)
+    // Correction due to time discretization
+    double N_double = (target_dist / (.02*ref_vel/2.24));
+    int N = floor(N_double);
+    target_x *= N / N_double;
+    target_y = s(target_y);
+    target_dist = sqrt(pow(target_x,2) + pow(target_y,2));
+    double angle = atan2(target_y, target_x);
+
+    // Final conditions
+    vector<double> end {target_dist, ref_vel / 2.24, 0.};
+
+    // Jerk minimizing trajectory
+    double T = (target_dist / ref_vel * 2.24); // good only for constant speed
+    vector<double> coeffs = JMT(start, end, T);
+
+    for(int i=1; i <= 50-car.previous_path_size; ++i)
     {
-        double N = (target_dist / (.02*ref_vel/2.24));
-        double x_point = x_add_on + (target_x)/N;
+        t_add_on += 0.02;
+
+        double x_point = polyeval(coeffs, t_add_on) * cos(angle);
         double y_point = s(x_point);
-        x_add_on = x_point;
 
         double x_ref = x_point;
         double y_ref = y_point;
 
-        // Rotate back
+        // Return to global coordinates
         x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
         y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
-
         x_point += ref_x;
         y_point += ref_y;
 
